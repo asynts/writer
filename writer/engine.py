879aefb37c6358a16d653518b9420bbd9a121513
Paul Scharnofske <asynts@gmail.com>
2022-05-26 13:21:38 +0200

    Writer: Generate a simple layout tree
    I think the next step would be to define size constraints and to do the
    word wrapping and the paragraph splitting.

--- writer/engine.py
+++ writer/engine.py
@@ -13,6 +13,9 @@ class LayoutTreeGenerator:
     # This can either find another region on the same page (multi-column layout) or
     # it can create a new page to provide the region.
     def next_region(self):
+        # FIXME: We should generalize this somehow, for header and footer.
+        #        In that case we want to return none, but not sure, how to approach this.
+
         self.current_region = self.current_page.add_content_node(layout.BlockLayoutNode())
 
     def next_page(self):
@@ -22,22 +25,43 @@ class LayoutTreeGenerator:
 
         if self.document.header_node is not None:
             layout_header_node = self.current_page.set_header_node(layout.BlockLayoutNode())
-            self.add_model_node(self.document.header_node, region=layout_header_node)
+            self.add_block_model_node(self.document.header_node, region=layout_header_node)
 
         if self.document.footer_node is not None:
             layout_footer_node = self.current_page.set_footer_node(layout.BlockLayoutNode())
-            self.add_model_node(self.document.footer_node, region=layout_footer_node)
+            self.add_block_model_node(self.document.footer_node, region=layout_footer_node)
 
-    def add_model_node(self, node: model.ModelNode, *, region: layout.LayoutNode = None):
+    def add_inline_model_node(self, node: model.ModelNode, *, container_node: layout.BlockLayoutNode):
+        # FIXME: We have to be able to split paragraphs here.
+
+        if isinstance(node, model.TextChunkModelNode):
+            # FIXME: Implement text wrapping here.
+            container_node.append_child(layout.TextLayoutNode(text=node.text))
+        elif isinstance(node, model.FieldChunkModelNode):
+            if node.field == "page_number":
+                container_node.append_child(layout.TextLayoutNode(text=str(len(self.pages))))
+            else:
+                raise NotImplementedError
+        else:
+            raise NotImplementedError
+
+    def add_block_model_node(self, node: model.ModelNode, *, region: layout.LayoutNode = None):
         if region is None:
             region = self.current_region
-        
-        # FIXME
+
+        if isinstance(node, model.ParagraphModelNode):
+            paragraph_node = layout.BlockLayoutNode()
+            region.append_child(paragraph_node)
+
+            for child_node in node.children:
+                self.add_inline_model_node(child_node, container_node=paragraph_node)
+        else:
+            raise NotImplementedError
 
 def generate_layout_tree(document: model.DocumentModelNode):
     generator = LayoutTreeGenerator(document)
 
-    for child_node in document.children:
-        generator.add_model_node(child_node)
+    for child_node in document.content_nodes:
+        generator.add_block_model_node(child_node)
 
     return generator.pages
