ce04e6c6925821274de92aac68fe0057a24fc6c9
Paul Scharnofske <asynts@gmail.com>
2022-05-27 13:19:00 +0200

    Writer: Reorganize code structure
    For now, I deleted everything that is currently unused, I can bring it
    back later.

--- writer/__main__.py
+++ writer/__main__.py
@@ -1,39 +1,13 @@
-import model
-import layout
-import engine
-
 import pygame
+
+import writer.engine.layout as layout
 
 COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0, 0, 0)
 COLOR_RED = (255, 0, 0)
 COLOR_GRAY = (200, 200, 200)
 
-def create_model_tree():
-    document = model.DocumentModelNode()
-
-    header_node_1 = document.set_header_node(model.ParagraphModelNode())
-
-    footer_node_1 = document.set_footer_node(model.ParagraphModelNode())
-
-    footer_node_1.append_child(model.TextChunkModelNode(text="Page "))
-    footer_node_1.append_child(model.FieldChunkModelNode(field="page_number"))
-    footer_node_1.append_child(model.TextChunkModelNode(text="more text that will hopefully overflow the footer."))
-
-    paragraph_1 = document.add_content_node(model.ParagraphModelNode())
-    paragraph_1.append_child(model.TextChunkModelNode(text="Hello, "))
-    paragraph_1.append_child(model.TextChunkModelNode(text="world"))
-    paragraph_1.append_child(model.TextChunkModelNode(text="!"))
-
-    paragraph_2 = document.add_content_node(model.ParagraphModelNode())
-    paragraph_2.append_child(model.TextChunkModelNode(text="This is another paragraph."))
-
-    return document
-
-def create_layout_tree_1(document: model.DocumentModelNode):
-    return engine.generate_layout_tree(document)
-
-def create_layout_tree_2():
+def create_layout_tree():
     layout_tree = layout.PageLayoutNode()
 
     # FIXME: Get actual paragraphs working.
@@ -65,7 +39,7 @@ def main():
     layout.normal_font = pygame.font.SysFont("monospace", 12)
     layout.font_width, layout.font_height = layout.normal_font.size("x")
 
-    layout_tree = create_layout_tree_2()
+    layout_tree = create_layout_tree()
 
     screen = pygame.display.set_mode([ 500, 500 ])
 
