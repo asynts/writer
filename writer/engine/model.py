76cb3733a946a9f03036ece89db5c24a757195a7
Paul Scharnofske <asynts@gmail.com>
2022-06-07 14:23:17 +0200

    Writer: Add text chunks into the model
    This is the first step towards formatting, because we will be able to
    define distinct styles for each text chunk.
    
    Currently, this doesn't work, because the logic that generates the
    layout nodes needs to be updated and that isn't a trivial task.

--- writer/engine/model.py
+++ writer/engine/model.py
@@ -5,19 +5,24 @@ class ModelNode:
 
     def add_child(self, child_node: "ModelNode"):
         self._children.append(child_node)
+        return child_node
 
-    def children(self):
+    def get_children(self):
         return self._children
 
 class DocumentModelNode(ModelNode):
     def __init__(self):
         super().__init__(name="DocumentModelNode")
 
-class ParagraphModelNode(ModelNode):
+class TextChunkModelNode(ModelNode):
     def __init__(self, *, text: str):
+        super().__init__(name="TextChunkModelNode")
+
+        self.__text = text
+    
+    def get_text(self):
+        return self.__text
+
+class ParagraphModelNode(ModelNode):
+    def __init__(self):
         super().__init__(name="ParagraphModelNode")
-
-        self._text = text
-
-    def get_text(self) -> str:
-        return self._text
