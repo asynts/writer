afee1dbb8f4bf9ff49a7c773887893a1aa0ceb63
Paul Scharnofske <asynts@gmail.com>
2022-06-07 09:22:24 +0200

    Writer: Create new pages to fit overflow

--- writer/engine/converter.py
+++ writer/engine/converter.py
@@ -1,5 +1,6 @@
 from . import model, layout
 
+# Returns the words that did not fit into this parent node.
 def generate_layout_nodes_for_words_helper(*, parent_node: layout.BlockLayoutNode, words: list[str]):
     # We do add the node here, but we might never place it, that is fine.
     new_layout_node = layout.BlockLayoutNode()
@@ -14,8 +15,6 @@ def generate_layout_nodes_for_words_helper(*, parent_node: layout.BlockLayoutNod
         if occupied_space > remaining_space:
             # Not enough space to fit this node, overflow to next page.
 
-            # FIXME: Actually create a new page.
-            print("page overflow")
             return False
         else:
             # We can safely place this node without causing overflow.
@@ -59,14 +58,18 @@ def generate_layout_nodes_for_words_helper(*, parent_node: layout.BlockLayoutNod
     else:
         return words[:]
 
+# Returns the words that did not fit into this parent node.
 def generate_layout_nodes_for_words(*, parent_node: layout.BlockLayoutNode, words: list[str]):
     new_layout_node = layout.BlockLayoutNode()
     new_layout_node.on_added_to_node(parent_node=parent_node)
 
     # Break down the problem and treat each line recursively.
-    generate_layout_nodes_for_words_helper(parent_node=new_layout_node, words=words)
+    # Returns the words that did not fit into this parent node.
+    words = generate_layout_nodes_for_words_helper(parent_node=new_layout_node, words=words)
 
     parent_node.place_block_node(new_layout_node)
+
+    return words
 
 def generate_layout_tree(model_tree: model.DocumentModelNode) -> layout.LayoutNode:
     layout_tree = layout.BlockLayoutNode()
@@ -76,16 +79,20 @@ def generate_layout_tree(model_tree: model.DocumentModelNode) -> layout.LayoutNo
 
     for paragraph in model_tree.children():
         assert isinstance(paragraph, model.ParagraphModelNode)
-
-        # FIXME: This is suboptimal, because we need to be able to pass information about the model node.
         words = paragraph.get_text().split()
-        generate_layout_nodes_for_words(parent_node=current_page_node.get_content_node(), words=words)
 
-    layout_tree.place_block_node(current_page_node)
+        # Returns the words that did not fit into this layout node and thus need to be placed on a new page.
+        words = generate_layout_nodes_for_words(parent_node=current_page_node.get_content_node(), words=words)
 
-    # Let's add another page for testing.
-    current_page_node = layout.PageLayoutNode()
-    current_page_node.on_added_to_node(layout_tree)
+        while len(words) >= 1:
+            # We had some overflow and need to create a new page to fit it.
+
+            layout_tree.place_block_node(current_page_node)
+            current_page_node = layout.PageLayoutNode()
+            current_page_node.on_added_to_node(layout_tree)
+
+            words = generate_layout_nodes_for_words(parent_node=current_page_node.get_content_node(), words=words)
+
     layout_tree.place_block_node(current_page_node)
 
     return layout_tree
