68cfe30a2f513d42841fe6ba36f9c24bffcc5c96
Paul Scharnofske <asynts@gmail.com>
2022-06-07 15:38:59 +0200

    Writer: Create outline for placement algorithm
    This problem is much more complicated than I thought.  I hope this
    covers all the mandatory edge cases.

--- writer/engine/converter.py
+++ writer/engine/converter.py
@@ -1,4 +1,73 @@
+import typing
 from . import model, layout
+
+import dataclasses
+
+# This represents part of a 'TextChunkModelNode' this is necessary, because these nodes can contain
+# spaces and thus contain multiple words.
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class TextExcerpt:
+    text_chunk_model_node: model.TextChunkModelNode
+    text: str
+
+# One rendered word could belong to multiple model nodes with different formatting.
+# They are obviously rendered separately, however, for text wrapping we need to consider them together.
+# This class represents one rendered word with everything that belongs to it.
+#
+# In the future, we could implement hypernation and this would be a good place to start.
+# But for now, we just place entire words.
+class WordGroup:
+    def __init__(self):
+        self.excerpts: list[TextExcerpt] = []
+        self.width = 0.0
+        self.height = 0.0
+
+def place_paragraph(paragraph_model_node: model.ParagraphModelNode):
+    # This is the rough algorithm that I have in mind.
+    # The idea is to construct word groups which represent the things being wrapped.
+    # Maybe, we could even do this in advance and then just do the placement in a separate step. <-- THIS SEEMS GOOD
+
+    # At some point, we need to store enough information to be able to find the model node from the layout node.
+    # On top of that, the model node needs to know the layout nodes that were generated for it.
+    # Maybe, not all layout nodes need to know the exact position, they only need to know what needs to be discarded when things are changed.
+    # When all of that works, I finally need to start working on the editing, that will be fun.
+
+    # NEW IDEA:
+
+    # Loop throuh all the text chunk in this paragraph.
+
+        # Construct text excerpts.
+
+        # Add text excerpt to current word group, creating new ones as needed.
+    
+    # Loop through the word groups.
+
+        # Try to fit the current word group into the current line.
+
+            # If it fits, move on and place the next word group.
+
+            # Otherwise, place the old line and create a new one to place the group in.
+
+                # If the old line doesn't fit, we create a new page and continue the placement there.
+                # In order to be able to do this, we need to keep track of the word groups that are being placed in the current line.
+                # The width of the new area could be different.
+
+    # OLD IDEA:
+
+    # Loop through all the text chunks in this paragraph.
+
+        # Construct text excerpts until a word group is completed.
+
+        # Try to fit the word group onto the current line.
+
+            # If fit, move on and construct the next word group.
+
+            # Otherwise, place the old line, and create a new line.
+
+                # If the old line doesn't fit on this page, create a new page.
+                # FIXME: In that case we would have to redo the layout, no?
+
+### OLD IMPLEMENTATION
 
 # Returns the words that did not fit into this parent node.
 def generate_layout_nodes_for_words_helper(*, parent_node: layout.BlockLayoutNode, words: list[str]):
