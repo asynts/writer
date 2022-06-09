dbb9372f3ad921f2eb497a1ef01fc52369757521
Paul Scharnofske <asynts@gmail.com>
2022-06-09 08:53:48 +0200

    Writer: Write unit tests for 'partition_remaining_text'
    Currently, that function hasn't been implemented though.

---
+++ writer/engine/tests/test_partition_remaining_text.py
@@ -0,0 +1,44 @@
+import dataclasses
+import typing
+import pytest
+
+import writer.engine.converter
+
+@dataclasses.dataclass(kw_only=True, frozen=True)
+class Case:
+    input_: str
+    expected: typing.Tuple[str, str, str]
+
+cases = [
+    Case(
+        input_="hello world",
+        expected=("hello", " ", "world")
+    ),
+    Case(
+        input_="hello  world",
+        expected=("hello", "  ", "world")
+    ),
+    Case(
+        input_=" hello world",
+        expected=("", " ", "hello world")
+    ),
+    Case(
+        input_="  hello world",
+        expected=("", "  ", "hello world")
+    ),
+    Case(
+        input_="hello\t world",
+        expected=("hello", "\t ", "world")
+    ),
+    Case(
+        input_=" \nhello\t world",
+        expected=("", " \n", "hello world")
+    ),
+]
+
+@pytest.fixture(params=cases)
+def case(request):
+    return request.param
+
+def test_partition_remaining_text(case):
+    assert writer.engine.converter.partition_remaining_text(case.input_) == case.expected
