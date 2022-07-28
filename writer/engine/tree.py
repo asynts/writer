370be9147971427f7e0e9b63f452b6708edee8c0
Paul Scharnofske <asynts@gmail.com>
2022-07-28 09:26:25 +0200

    Writer: Implement the new immutable tree logic in the actual codebase
    Now, I should be able to update the calling code to take advantage of
    the keys to reference other nodes.

--- writer/engine/tree.py
+++ writer/engine/tree.py
@@ -1,5 +1,18 @@
 import copy
 
+# Each node is assigned a unique id on creation.
+# When we want to change a node we create a copy but keep the same key.
+# Therefore, a list of keys can be used to identify a node in the tree while allowing modifications.
+_next_node_key = 1
+def create_node_key():
+    global _next_node_key
+    key = _next_node_key
+    _next_node_key += 1
+    return key
+
+class NodeNotFound(Exception):
+    pass
+
 # At first nodes are mutable.
 # However, they must become immutable before they can can become children of other nodes.
 # Once immutable, they can not become mutable again.
@@ -7,11 +20,15 @@ class Node:
     __slots__ = (
         "__children",
         "__is_mutable",
+        "__key",
     )
 
     def __init__(self, *, children: list["Node"]):
         # Property.
         self.__children = children
+
+        # Property.
+        self.__key = create_node_key()
 
         self.__is_mutable = True
 
@@ -25,6 +42,39 @@ class Node:
         copy_.__is_mutable = True
         return copy_
 
+    # Finds a node in the subtree spanned by this node.
+    def lookup_node_recursively(self, *, key_path: list[int]) -> "Node":
+        if len(key_path) == 0:
+            return self
+
+        for child_node in self.children:
+            if child_node.key == key_path[0]:
+                return child_node.lookup_node_recursively(key_path=key_path[1:])
+
+        raise NodeNotFound
+
+    # Replace a node in the subtree spanned by this node.
+    # Returns a copy of the current node with updated parents.
+    # If they key path references this node, returns the new node.
+    def replace_node_recursively(self, *, key_path: list[int], new_node: "Node") -> "Node":
+        if len(key_path) == 0:
+            return new_node
+
+        for child_index, child_node in enumerate(self.children):
+            if child_node.key == key_path[0]:
+                mutable_copy = self.make_mutable_copy()
+
+                mutable_copy.children = [
+                    self.children[:child_index],
+                    child_node.replace_node_recursively(key_path=key_path[1:], new_node=new_node),
+                    self.children[child_index+1:],
+                ]
+
+                mutable_copy.make_immutable()
+                return mutable_copy
+
+        raise NodeNotFound
+
     def append_child(self, child_node: "Node"):
         assert self.is_mutable
         self.__children.append(child_node)
@@ -37,7 +87,7 @@ class Node:
     def dump(self, *, name: str = "Node", indent: int = 0):
         result = ""
         result += " " * indent
-        result += f"{name}@{id(self)}("
+        result += f"{name}@{self.key}("
         result += self.dump_properties()
         result += ")\n"
 
@@ -53,6 +103,10 @@ class Node:
     @property
     def children(self):
         return self.__children
+
+    @property
+    def key(self):
+        return self.__key
 
     @children.setter
     def children(self, value: list["Node"]):
