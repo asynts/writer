3f0e5b757ac7e997c313fa70566947da2961daf4
Paul Scharnofske <asynts@gmail.com>
2022-08-23 10:13:50 +0200

    Writer: Compute the word placement instructions

--- writer/engine/tests/test_text_placement.py
+++ writer/engine/tests/test_text_placement.py
@@ -2,7 +2,7 @@ import writer.engine.text_placement as text_placement
 import writer.engine.model as model
 
 def test_algorithm_terminates_1():
-    text_placement.compute_placement_instructions_for_paragraph(
+    placement_instructions = text_placement.compute_placement_instructions_for_paragraph(
         model.ParagraphModelNode(
             style=None,
             children=[
@@ -14,10 +14,12 @@ def test_algorithm_terminates_1():
                 ),
                 model.TextChunkModelNode(
                     style=None,
-                    text="More text.",
+                    text=" More text.",
                     cursor_offset=None,
                     children=[],
                 ),
             ],
         ),
     )
+
+    text_placement.print_placement_instructions(placement_instructions)
