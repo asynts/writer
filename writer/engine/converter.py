160eb7918acb8a94168946dacdef5ae21135357d
Paul Scharnofske <asynts@gmail.com>
2022-08-26 13:04:10 +0200

    [WIP] Writer: Implement the logic that places paragraphs
    This already looks much better than the convoluted mess before, however,
    it does not appear to be working at the moment.

--- writer/engine/converter.py
+++ writer/engine/converter.py
@@ -51,7 +51,7 @@ class LayoutGenerator:
             ),
         )
 
-    def new_pending_paragraph(self, paragraph_model_node: model.ParagraphModelNode):
+    def new_pending_paragraph(self, *, paragraph_model_node: model.ParagraphModelNode):
         assert self.pending_paragraph_layout_node is None
 
         self.pending_paragraph_layout_node = layout.VerticalLayoutNode(
@@ -63,9 +63,96 @@ class LayoutGenerator:
         )
 
     def place_paragraph(self, paragraph_model_node: model.ParagraphModelNode):
-        placement_instructions = text_placement.compute_placement_instructions_for_paragraph(paragraph_model_node)
+        remaining_instructions = text_placement.compute_placement_instructions_for_paragraph(paragraph_model_node)
 
-        # FIXME: Magic.
+        self.new_pending_paragraph(paragraph_model_node=paragraph_model_node)
+
+        pending_cursor_instruction: text_placement.CursorPlacementInstruction = None
+
+        while len(remaining_instructions) >= 1:
+            # Place one line at a time.
+
+            max_line_width = self.pending_paragraph_layout_node.get_max_inner_width()
+
+            current_line_instructions: list[text_placement.PlacementInstruction] = []
+            current_line_width = 0.0
+
+            current_line_height = 0.0 # FIXME
+
+            while len(remaining_instructions) >= 1:
+                instruction = remaining_instructions.pop(0)
+
+                if isinstance(instruction, text_placement.WordPlacementInstruction):
+                    if util.approximately_less(current_line_width + instruction.width, max_line_width):
+                        # Place cursor if necessary.
+                        if pending_cursor_instruction is not None:
+                            current_line_instructions.append(pending_cursor_instruction)
+                            pending_cursor_instruction = None
+
+                        # Word does fit in the current line, continue.
+                        current_line_instructions.append(instruction)
+                        current_line_width += instruction.width
+                        continue
+                    else:
+                        # Word does not fit in current line, break.
+                        remaining_instructions.insert(0, instruction)
+                        break
+                elif isinstance(instruction, text_placement.WhitespacePlacementInstruction):
+                    if util.approximately_less(current_line_width + instruction.width, max_line_width):
+                        # Place cursor if necessary.
+                        if pending_cursor_instruction is not None:
+                            current_line_instructions.append(pending_cursor_instruction)
+                            pending_cursor_instruction = None
+
+                        # Whitespace does fit in the current line, continue.
+                        current_line_instructions.append(instruction)
+                        current_line_width += instruction.width
+                    else:
+                        if pending_cursor_instruction is None:
+                            # Whitespace does not fit in current line, break.
+                            # But we do not queue it again, since a new line is essentially whitespace.
+                            break
+                        else:
+                            # Whitespace does not fit in current line, break.
+                            # But we do queue again, because the cursor will otherwise be rendered incorrectly.
+                            remaining_instructions.insert(0, instruction)
+                            break
+                elif isinstance(instruction, text_placement.CursorPlacementInstruction):
+                    assert pending_cursor_instruction is None
+
+                    # Cursor can always be placed, but delay placement.
+                    pending_cursor_instruction = instruction
+                    continue
+                else:
+                    raise AssertionError
+
+            if pending_cursor_instruction is not None:
+                # It can happen, that the cursor is placed at the very end.
+                if len(remaining_instructions) == 0:
+                    current_line_instructions.append(pending_cursor_instruction)
+                    pending_cursor_instruction = None
+
+            # Not enough space in this paragraph, create a new one.
+            if util.approximately_greater(current_line_height, self.pending_paragraph_layout_node.get_max_remaining_height()):
+                self.new_pending_paragraph(paragraph_model_node=paragraph_model_node)
+
+                # We assume that the new paragraph layout node has the same width, which should be reasonable.
+                assert self.pending_paragraph_layout_node.get_max_inner_width() == max_line_width
+
+            line_layout_node = layout.HorizontalLayoutNode(
+                parent_node=self.pending_paragraph_layout_node,
+                model_node=None,
+                style=layout.LayoutStyle(),
+            )
+
+            # FIXME: Now, we need to create all the 'TextChunkLayoutNode' nodes.
+
+            self.pending_paragraph_layout_node.place_child_node(line_layout_node)
+
+            # We are now done with that line, however, we might carry a 'pending_cursor_instruction' into the next line.
+            continue
+
+        self._try_place_pending_paragraph()
 
     def finalize(self) -> layout.LayoutNode:
         self._try_place_pending_paragraph()
