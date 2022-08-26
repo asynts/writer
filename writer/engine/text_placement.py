a1a89d899c2aa1b16f397dd21a3f40f85edf2402
Paul Scharnofske <asynts@gmail.com>
2022-08-26 13:04:10 +0200

    Writer: Start implementing the logic that places paragraphs
    This already looks much better than the convoluted mess before.
    However, there are a few things that we are not yet doing:
    
    -   Caching.
    
    -   Generating the actual 'TextChunkLayoutNode' objects.

--- writer/engine/text_placement.py
+++ writer/engine/text_placement.py
@@ -7,58 +7,92 @@
 
 # FIXME: Provide some examples where weird things need to happen.
 
+from PyQt6 import QtCore
+
 import dataclasses
 import string
 
 import writer.engine.model as model
 
-@dataclasses.dataclass(frozen=True, kw_only=True, slots=True)
-class TextExcerpt:
+@dataclasses.dataclass(kw_only=True, slots=True)
+class SizeMixin:
+    __size: QtCore.QSizeF = None
+
+    def get_size(self):
+        raise NotImplementedError
+
+    @property
+    def width(self):
+        if self.__size is None:
+            self.__size = self.get_size()
+        return self.__size.width()
+
+    @property
+    def height(self):
+        if self.__size is None:
+            self.__size = self.get_size()
+        return self.__size.height()
+
+# Invariant: Immutable after creation.
+@dataclasses.dataclass(kw_only=True, slots=True)
+class TextExcerpt(SizeMixin):
     model_node: "model.TextChunkModelNode"
     model_offset: int
     text: str
     style_cascade: "model.ModelStyleCascade"
 
-    def get_width(self):
-        width, height = self.style_cascade.font_metrics.size(0, self.text)
-        return width
+    def get_size(self):
+        return self.style_cascade.font_metrics.size(0, self.text)
 
-@dataclasses.dataclass(frozen=True, kw_only=True, slots=True)
+# Invariant: Immutable after creation.
+@dataclasses.dataclass(kw_only=True, slots=True)
 class PlacementInstruction:
     pass
 
-@dataclasses.dataclass(frozen=True, kw_only=True, slots=True)
+# Invariant: Immutable after creation.
+@dataclasses.dataclass(kw_only=True, slots=True)
 class WordPlacementInstruction(PlacementInstruction):
     excerpts: list[TextExcerpt]
     __width: float = None
+    __height: float = None
 
     @property
     def width(self):
         if self.__width is None:
-            self.__width = sum(excerpt.get_width() for excerpt in self.excerpts)
+            self.__width = sum(excerpt.width for excerpt in self.excerpts)
         return self.__width
 
+    @property
+    def height(self):
+        if self.__height is None:
+            self.__height = max(excerpt.height for excerpt in self.excerpts)
+        return self.__height
+
 # Indicates that spacing should be added before the next word is placed.
-@dataclasses.dataclass(frozen=True, kw_only=True, slots=True)
-class WhitespacePlacementInstruction(PlacementInstruction):
+#
+# Invariant: Immutable after creation.
+@dataclasses.dataclass(kw_only=True, slots=True)
+class WhitespacePlacementInstruction(PlacementInstruction, SizeMixin):
     model_node: "model.TextChunkModelNode"
     model_offset: "model.TextChunkModelNode"
     style_cascade: "model.ModelStyleCascade"
-    __width: float = None
 
-    @property
-    def width(self):
-        if self.__width is None:
-            self.__width, _ = self.style_cascade.font_metrics.size(0, " ")
-        return self.__width
+    def get_size(self):
+        return self.style_cascade.font_metrics.size(0, " ")
 
 # Indicates that the cursor should be rendered when the next word is placed.
 # This happens if the cursor is placed at the end of a node after whitespace.
 # In that case it doesn't belong to the previous word and sticks to the next one.
-@dataclasses.dataclass(frozen=True, kw_only=True, slots=True)
-class CursorPlacementInstruction(PlacementInstruction):
+#
+# Invariant: Immutable after creation.
+@dataclasses.dataclass(kw_only=True, slots=True)
+class CursorPlacementInstruction(PlacementInstruction, SizeMixin):
     model_node: "model.TextChunkModelNode"
     model_offset: int
+
+    def get_size(self):
+        # The cursor does not reserve any space.
+        return QtCore.QSizeF(0, 0)
 
 # This works similar to 'str.partition'.
 # The string is split into three parts, the text before the separator, the separator and the text after.
