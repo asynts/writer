4d5a004906a4edd01a28e75a21619d4153e9e382
Paul Scharnofske <asynts@gmail.com>
2023-01-21 15:20:04 +0100

    Tests: Test 'NodePath.replace'
    I found one edge case that doesn't work at the moment.  If the first key
    doesn't match what it's expecting, it asserts.

--- writer/engine/tests/test_tree.py
+++ writer/engine/tests/test_tree.py
@@ -24,9 +24,22 @@ class NamedNode(tree.Node):
     def name(self):
         return self.__name
 
+    def is_equivalent_to(self, other_node: "NamedNode"):
+        if self.name != other_node.name:
+            return False
+
+        if len(self.children) != len(other_node.children):
+            return False
+
+        for child, other_child in zip(self.children, other_node.children):
+            if not child.is_equivalent_to(other_child):
+                return False
+
+        return True
+
 @dataclass
 class TreeFixture:
-    tree: NamedNode
+    root_node: NamedNode
     lookup_node: dict[str, NamedNode]
     lookup_key: dict[str, int]
 
@@ -86,7 +99,7 @@ def fixture_default_tree():
     )
     node_a.make_immutable()
 
-    tree = node_a
+    root_node = node_a
     lookup_node = {
         "a": node_a,
         "b": node_b,
@@ -105,10 +118,30 @@ def fixture_default_tree():
     }
 
     return TreeFixture(
-        tree=tree,
+        root_node=root_node,
         lookup_node=lookup_node,
         lookup_key=lookup_key,
     )
+
+@pytest.fixture
+def fixture_node_y():
+    node = NamedNode(
+        name="y",
+        children=[],
+    )
+    node.make_immutable()
+    return node
+
+@pytest.fixture
+def fixture_node_x(fixture_node_y: tree.Node):
+    node = NamedNode(
+        name="x",
+        children=[
+            fixture_node_y,
+        ],
+    )
+    node.make_immutable()
+    return node
 
 def test_immutable_raises_exception():
     node_1 = tree.Node(children=[])
@@ -142,7 +175,7 @@ def test_lookup_root_node(fixture_default_tree: TreeFixture):
         fixture_default_tree.lookup_key["a"],
     ])
 
-    assert node_path.lookup(root_node=fixture_default_tree.tree) is fixture_default_tree.lookup_node["a"]
+    assert node_path.lookup(root_node=fixture_default_tree.root_node) is fixture_default_tree.lookup_node["a"]
 
 def test_lookup_nested_node_1(fixture_default_tree: TreeFixture):
     node_path = tree.NodePath([
@@ -151,7 +184,7 @@ def test_lookup_nested_node_1(fixture_default_tree: TreeFixture):
         fixture_default_tree.lookup_key["d"],
     ])
 
-    assert node_path.lookup(root_node=fixture_default_tree.tree) is fixture_default_tree.lookup_node["d"]
+    assert node_path.lookup(root_node=fixture_default_tree.root_node) is fixture_default_tree.lookup_node["d"]
 
 def test_lookup_nested_node_2(fixture_default_tree: TreeFixture):
     node_path = tree.NodePath([
@@ -159,7 +192,7 @@ def test_lookup_nested_node_2(fixture_default_tree: TreeFixture):
         fixture_default_tree.lookup_key["e"],
     ])
 
-    assert node_path.lookup(root_node=fixture_default_tree.tree) is fixture_default_tree.lookup_node["e"]
+    assert node_path.lookup(root_node=fixture_default_tree.root_node) is fixture_default_tree.lookup_node["e"]
 
 def test_lookup_invalid_path(fixture_default_tree: TreeFixture):
     node_path = tree.NodePath([
@@ -168,4 +201,198 @@ def test_lookup_invalid_path(fixture_default_tree: TreeFixture):
     ])
 
     with pytest.raises(tree.NodeNotFound):
-        node_path.lookup(root_node=fixture_default_tree.tree)
+        node_path.lookup(root_node=fixture_default_tree.root_node)
+
+def test_replace_root(fixture_default_tree: TreeFixture):
+    node_path = tree.NodePath([
+        fixture_default_tree.lookup_key["a"],
+    ])
+
+    new_node = NamedNode(
+        name="x",
+        children=[],
+    )
+    new_node.make_immutable()
+
+    new_root_node = node_path.replace(new_node, root_node=fixture_default_tree.root_node)
+
+    assert new_root_node.is_equivalent_to(new_node)
+
+@pytest.fixture
+def fixture_nested_1_tree():
+    # <node name="a">
+    #     <node name="b">
+    #         <node name="c" />
+    #         <node name="d" />
+    #     </node>
+    #     <node name="x">
+    #         <node name="y" />
+    #     </node>
+    # </node>
+
+    node_y = NamedNode(
+        name="y",
+        children=[],
+    )
+    node_y.make_immutable()
+
+    node_x = NamedNode(
+        name="x",
+        children=[
+            node_y,
+        ],
+    )
+    node_x.make_immutable()
+
+    node_d = NamedNode(
+        name="d",
+        children=[],
+    )
+    node_d.make_immutable()
+
+    node_c = NamedNode(
+        name="c",
+        children=[],
+    )
+    node_c.make_immutable()
+
+    node_b = NamedNode(
+        name="b",
+        children=[
+            node_c,
+            node_d,
+        ],
+    )
+    node_b.make_immutable()
+
+    node_a = NamedNode(
+        name="a",
+        children=[
+            node_b,
+            node_x,
+        ],
+    )
+    node_a.make_immutable()
+
+    root_node = node_a
+    lookup_node = {
+        "a": node_a,
+        "b": node_b,
+        "c": node_c,
+        "d": node_d,
+        "x": node_x,
+        "y": node_y,
+    }
+    lookup_key = {
+        "a": node_a.key,
+        "b": node_b.key,
+        "c": node_c.key,
+        "d": node_d.key,
+        "x": node_x.key,
+        "y": node_y.key,
+    }
+
+    return TreeFixture(
+        root_node=root_node,
+        lookup_node=lookup_node,
+        lookup_key=lookup_key,
+    )
+
+def test_replace_nested_1(fixture_default_tree: TreeFixture, fixture_nested_1_tree: TreeFixture, fixture_node_x: tree.Node):
+    node_path = tree.NodePath([
+        fixture_default_tree.lookup_key["a"],
+        fixture_default_tree.lookup_key["e"],
+    ])
+
+    new_root_node = node_path.replace(fixture_node_x, root_node=fixture_default_tree.root_node)
+
+    assert fixture_nested_1_tree.root_node.is_equivalent_to(new_root_node)
+
+@pytest.fixture
+def fixture_nested_2_tree():
+    # <node name="a">
+    #     <node name="y" />
+    #     <node name="e">
+    #         <node name="f" />
+    #     </node>
+    # </node>
+
+    node_f = NamedNode(
+        name="f",
+        children=[],
+    )
+    node_f.make_immutable()
+
+    node_e = NamedNode(
+        name="e",
+        children=[
+            node_f,
+        ],
+    )
+    node_e.make_immutable()
+
+    node_y = NamedNode(
+        name="y",
+        children=[],
+    )
+    node_y.make_immutable()
+
+    node_a = NamedNode(
+        name="a",
+        children=[
+            node_y,
+            node_e,
+        ],
+    )
+    node_a.make_immutable()
+
+    root_node = node_a
+    lookup_node = {
+        "a": node_a,
+        "y": node_y,
+        "e": node_e,
+        "f": node_f,
+    }
+    lookup_key = {
+        "a": node_a.key,
+        "y": node_y.key,
+        "e": node_e.key,
+        "f": node_f.key,
+    }
+
+    return TreeFixture(
+        root_node=root_node,
+        lookup_node=lookup_node,
+        lookup_key=lookup_key,
+    )
+
+def test_replace_nested_2(fixture_default_tree: TreeFixture, fixture_nested_2_tree: TreeFixture, fixture_node_y: tree.Node):
+    node_path = tree.NodePath([
+        fixture_default_tree.lookup_key["a"],
+        fixture_default_tree.lookup_key["b"],
+    ])
+
+    new_root_node = node_path.replace(fixture_node_y, root_node=fixture_default_tree.root_node)
+
+    print(fixture_nested_2_tree.root_node.dump())
+    print(new_root_node.dump())
+
+    assert fixture_nested_2_tree.root_node.is_equivalent_to(new_root_node)
+
+def test_replace_not_found(fixture_default_tree: TreeFixture, fixture_node_y: tree.Node):
+    node_path = tree.NodePath([
+        fixture_default_tree.lookup_key["a"],
+        fixture_default_tree.lookup_key["b"],
+        fixture_default_tree.lookup_key["b"],
+    ])
+
+    with pytest.raises(tree.NodeNotFound):
+        node_path.replace(fixture_node_y, root_node=fixture_default_tree.root_node)
+
+# def test_replace_not_found_root(fixture_default_tree: TreeFixture, fixture_node_y: tree.Node):
+#     node_path = tree.NodePath([
+#         123,
+#     ])
+
+#     with pytest.raises(tree.NodeNotFound):
+#         node_path.replace(fixture_node_y, root_node=fixture_default_tree.root_node)
